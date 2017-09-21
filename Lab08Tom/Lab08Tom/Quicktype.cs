bff9ed4390ba610506ff63e263e16a76fad193ea
Thomas Reyes <thomasellison.r@gmail.com>
2017-09-21 12:19:12 -0700

    Reading json file and first 2 questions done

---
+++ Lab08Tom/Lab08Tom/Quicktype.cs
@@ -0,0 +1,51 @@
+﻿// To parse this JSON data, add NuGet 'Newtonsoft.Json' then do:
+//
+//    using QuickType;
+//
+//    var data = GettingStarted.FromJson(jsonString);
+//
+// For POCOs visit quicktype.io?poco
+//
+namespace QuickType
+{
+    using System;
+    using System.Net;
+    using System.Collections.Generic;
+
+    using Newtonsoft.Json;
+
+    public partial class GettingStarted
+    {
+        [JsonProperty("greeting")]
+        public string Greeting { get; set; }
+
+        [JsonProperty("instructions")]
+        public string[] Instructions { get; set; }
+    }
+
+
+    public partial class GettingStarted
+    {
+        public static GettingStarted FromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<GettingStarted>(json, Converter.Settings);
+        }
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this GettingStarted self)
+        {
+            return JsonConvert.SerializeObject(self, Converter.Settings);
+        }
+    }
+
+    public class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+        };
+    }
+}
